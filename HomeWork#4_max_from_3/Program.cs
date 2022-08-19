31626362c655ceb5f51803e58331561eb54e7c33
Alex <donerlux@mail.ru>
2022-08-19 15:13:12 +0300

    Add HomeWork 4: Максимальное из трех чисел

---
+++ HomeWork#4_max_from_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Факультет: Разработчик           Студент: Ященко Алексей
+// Задача №4: Написать программу,которая принимает на вход три числа 
+//            и выдает максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("Максимальное число из трех введенных чисел = ");
+Console.WriteLine(max);

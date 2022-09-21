e0461df1ae03318239bb9c0a62b0b8100effe0d3
Alex <donerlux@mail.ru>
2022-09-21 10:59:17 +0300

    Add Sem 9

---
+++ Sem_9(не проверена)/Work№66_Recurs_Sum_M_N/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №66: Задать значения М и N. Написать программу, которая найдет сумму натуральных
+//             эл-тов в промежутке от М до N.
+
+int NumberSum(int a, int b)
+{
+    if (b == a) return a;
+    else return b + NumberSum(a, b - 1);
+}
+
+Console.WriteLine("Введите число M: ");
+int start = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int end = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Sum of numbers from {start} to {end} = {NumberSum(start, end)}");
---
+++ Sem_9(не проверена)/Work№68_Akkerman/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №68: Написать программу вычисления функции Аккермана с помошью рекурсиию.
+// Даны два неотрицательных числа m и n.
+
+int FunctionАkkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return FunctionАkkerman(m - 1, 1);
+    return FunctionАkkerman(m - 1, FunctionАkkerman(m, n - 1));
+}
+Console.WriteLine("Enter number m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number n: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"FunctionАkkerman({m}, {n}) = {FunctionАkkerman(m, n)}");
